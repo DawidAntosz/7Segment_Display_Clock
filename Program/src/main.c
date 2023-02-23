e8e2435e0fb6304b6cf2712611356d097eaf609c
DawidAntosz <antoszdawid@student.agh.edu.pl>
2023-02-23 06:17:07 +0100

    main edit

--- Program/src/main.c
+++ Program/src/main.c
@@ -8,36 +8,29 @@
 #include <avr/pgmspace.h>
 
 #include "MAX7219.h"
-
-uint8_t hour = 0;
-uint8_t minute = 0;
-uint8_t second = 0;
+#include "ds1307.h"
 
 
 int main(void)
 {
     MAX7219_init();
+    DS1307_init();
+    
+    uint8_t year = 0;
+	uint8_t month = 0;
+	uint8_t day = 0;
+	uint8_t hour = 0;
+	uint8_t minute = 0;
+	uint8_t second = 0;
 
+    DS1307_setdate(23, 2, 23, 6, 5, 40);
 
     while(1)
     {
-        if(second > 59){
-            second =0;
-            minute +=1;
-        }
-        if(minute > 59){
-            minute =0;
-            hour += 1;
-        }
-        if(hour > 23){
-            hour = 0;
-            second = 0;
-            minute = 0;
-        }
-        second++;
-        _delay_ms(100);
+        DS1307_getdate(&year, &month, &day, &hour, &minute, &second);
 
         MAX7219_displayClock(&hour, &minute, &second);
-        
+
+        _delay_ms(10);
     }
 }
